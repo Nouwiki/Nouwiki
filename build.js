67317c4c7525bd073340588b19c28d774da775aa
Brynjar Harðarson <brynjar@gmail.com>
2015-10-30 18:46:25 +0000

    verbos front matter parsing

--- build.js
+++ build.js
@@ -28,6 +28,7 @@ var md = require('markdown-it')({
 function build(markup_file) {
 	var data = fs.readFileSync(markup_file, 'utf8');
 	var content = matter(data, { lang: 'toml', delims: ['+++', '+++']});
+  console.log(content)
 	markData = content.content;
 	if (content.data.title != undefined) {
 		markData = "# "+content.data.title+"\n\n"+markData
